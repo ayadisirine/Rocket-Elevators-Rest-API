27a6db9be843d04cc3c3dfcd750e33cef4093759
ayadisirine <sirine.ayadi93@gmail.com>
2021-03-16 12:52:47 -0600

    all sirine tasks are finished

--- Models/Elevators.cs
+++ Models/Elevators.cs
@@ -9,18 +9,8 @@ namespace Rocket_Elevators_Rest_API.Models
     public partial class Elevators
     {
         public long Id { get; set; }
-        public string SerialNumber { get; set; }
-        public string Model { get; set; }
-        public string Status { get; set; }
-        public DateTime DateOfCommissioning { get; set; }
-        public DateTime DateOfLastInspection { get; set; }
-        public string CertificateOfInspection { get; set; }
-        public string Information { get; set; }
-        public string Notes { get; set; }
-        public long? ColumnId { get; set; }
-        public string ElevatorType { get; set; }
 
-        public virtual Columns Column { get; set; }
+        public string Status { get; set; }
 
         internal AppDb Db { get; set; }
 
